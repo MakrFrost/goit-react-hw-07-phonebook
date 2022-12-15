a2333dde19fbefcce479def267f71f866fa596fa
MakrFrost <frostmark@ukr.net>
2022-12-15 18:55:04 +0200

    upd

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,69 +1,75 @@
-import { useState, useEffect } from 'react';
+import { useGetContactsQuery } from './redux/contactsApi';
 
-import Form from './Form/Form';
-import Filter from './Filter/Filter';
-import Contacts from './Contacts/Contacts';
-import Section from './Section/Section';
+// import Form from './Form/Form';
+// import Filter from './Filter/Filter';
+// import Contacts from './Contacts/Contacts';
+// import Section from './Section/Section';
 
 function Phonebook() {
-  const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  // const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState([
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]);
 
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  // useEffect(() => {
+  //   const contacts = localStorage.getItem('contacts');
+  //   const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
+  //   if (parsedContacts) {
+  //     setContacts(parsedContacts);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    if (contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }, [contacts]);
+  // useEffect(() => {
+  //   if (contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(contacts));
+  //   }
+  // }, [contacts]);
 
-  const onFilterChange = value => {
-    setFilter(value);
-  };
-  const onAddContact = contact => {
-    if (contacts.filter(({ name }) => name === contact.name).length !== 0) {
-      alert(contact.name + 'is already in contacts!');
-      return;
-    }
+  // const onFilterChange = value => {
+  //   setFilter(value);
+  // };
+  // const onAddContact = contact => {
+  //   if (contacts.filter(({ name }) => name === contact.name).length !== 0) {
+  //     alert(contact.name + 'is already in contacts!');
+  //     return;
+  //   }
 
-    const newContact = {
-      ...contact,
-    };
-    setContacts([newContact, ...contacts]);
-  };
-  const onDeleteContact = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-  };
-  const filteredContact = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  };
+  //   const newContact = {
+  //     ...contact,
+  //   };
+  //   setContacts([newContact, ...contacts]);
+  // };
+  // const onDeleteContact = id => {
+  //   setContacts(contacts.filter(contact => contact.id !== id));
+  // };
+  // const filteredContact = () => {
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
+  // };
+
+  const { data, isLoading } = useGetContactsQuery();
+  console.log(data);
 
   return (
     <div>
-      <Section title={'Add contact to phonebook!'}>
-        <Form onAddContact={onAddContact} />
+      {isLoading ? (
+        <p>ничего не нашли, извините</p>
+      ) : (
+        <p>ваша записная книга</p>
+      )}
+
+      {/* <Section title={'Add contact to phonebook!'}>
+        <Form />
       </Section>
       <Section title={'Contacts:'}>
-        <Filter filter={filter} onFilterChange={onFilterChange} />
-        <Contacts
-          contacts={filteredContact()}
-          onDeleteContact={onDeleteContact}
-        />
-      </Section>
+        <Filter />
+        <Contacts />
+      </Section> */}
     </div>
   );
 }
---
+++ src/components/Phonebook/redux/contactsApi.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const contactsApi = createApi({
+  reducerPath: 'contactsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://6399d82016b0fdad774b4eb6.mockapi.io/contacts',
+  }),
+  endpoints: builder => ({
+    getContacts: builder.query({
+      query: () => '/Phonebook',
+    }),
+  }),
+});
+
+export const { useGetContactsQuery } = contactsApi;
